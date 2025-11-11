494d749362c7957e9d979d40227b63399e2b96d0
VladimirFabri98 <vfficial98@gmail.com>
2025-11-11 13:55:17 +0100

    Project template

---
+++ Projekat/src/geometry/Main.java
@@ -0,0 +1,10 @@
+package geometry;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Dobrodosli na vezbe iz Objektno orijentisanih tehnologija!");
+
+	}
+
+}
